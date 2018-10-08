cd870070b2b76f3295ba42a0f830d6188832b850
Kadahizi <jbatgoal@yahoo.fr>
2018-10-08 20:06:51 +0200

    changes in request index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180928064005) do
+ActiveRecord::Schema.define(version: 20181005132302) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
@@ -52,9 +52,9 @@ ActiveRecord::Schema.define(version: 20180928064005) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.string "first_name"
     t.string "last_name"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
