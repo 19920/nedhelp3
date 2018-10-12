9d55c45e4c222bf61c867c45081916afafa2c14b
Kadahizi <jbatgoal@yahoo.fr>
2018-10-12 16:12:38 +0200

    changes in stats page

--- app/assets/javascripts/components/SignUp.jsx
+++ app/assets/javascripts/components/SignUp.jsx
@@ -7,13 +7,12 @@ var SignUp = React.createClass({
          <div className="container">
 
             <div className="navbar navbar-expand-lg navbar-light bg-light  fixed-top">
-                    <a className="navbar-brand" href="/"><h4>Söderhamn Hjärtar</h4></a>
+                    <a className="navbar-brand" href="/"><h4>Söderhamn Hjältar</h4></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-item nav-link" href="/users/edit"><h6>Change Password</h6></a>
                             <a className="nav-item nav-link" href="/welcome_page/location"><h6>Location</h6></a>
                             <a className= "nav-item nav-link" href="/requests"><h6>My Requests</h6></a>
                              <a className="nav-item nav-link" href="/requests/new"><h6>Add Request</h6></a>
